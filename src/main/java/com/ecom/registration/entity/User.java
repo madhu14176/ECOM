8b80d605f7ebac71fb06926c16cfb7f64ca6011c
Madhu <madhu14176@gmail.com>
2025-07-13 00:46:02 +0530

    3nd commit for Config and API Validations

--- src/main/java/com/ecom/registration/entity/User.java
+++ src/main/java/com/ecom/registration/entity/User.java
@@ -2,11 +2,15 @@ package com.ecom.registration.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @DiscriminatorValue("USER")
 @Data
+
 public class User extends Account{
     private String mobile;
     private int age;
